eb537bef3d71c5252fd3e246276ae0b8572eb4f5
kuniyoshinakamura <74222652+kuniyoshinakamura@users.noreply.github.com>
2025-07-18 07:02:53 +0900

    Delete routes directory

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-use App\Http\Controllers\MessagesController;
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [MessagesController::class, 'index']);
-Route::resource('messages', MessagesController::class);
